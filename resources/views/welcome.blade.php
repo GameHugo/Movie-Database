56546e3c776edfe8143e6e009474bf83c0a0af93
Hugo Schoots <hschoots8224@student.landstede.nl>
2024-06-06 09:36:43 +0200

    Install laravel, tailwind, fortify and livewire

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Movie Database</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+<div class="w-full flex flex-col items-center">
+    <h1 class="font-medium text-3xl">Movie Database</h1>
+</div>
+</body>
+</html>
